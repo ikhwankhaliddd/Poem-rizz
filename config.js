d3ec959a10ec503aaa94e8332e2fcc747cee6f80
Ikhwan <muhammad.nasution@amartha.com>
2023-04-08 13:19:06 +0700

    init commit

---
+++ config.js
@@ -0,0 +1,8 @@
+var full_config = {
+    countdown: {
+        date: '2023-04-14 07:39:00',
+        foreground: '#fff',
+        background: 'rgba(0, 0, 0, .15)',
+    },
+    twitter: 'themewizz'
+};
